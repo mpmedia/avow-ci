ee4c665e9176af7950835219a12d8d4c1e2824cb
fluidbyte <kent@fluidbyte.net>
2014-03-24 00:54:05 +0000

    Adding branch selector to repo/project config

--- public/src/js/controllers/project/config.js
+++ public/src/js/controllers/project/config.js
@@ -13,6 +13,7 @@ define([
     name: ko.observable(),
     repo: ko.observable(),
     branch: ko.observable(),
+    availableBranches: ko.observableArray(),
 
     // Check if session exists
     before: function (fn) {
@@ -27,10 +28,15 @@ define([
     },
 
     load: function (name) {
+      var self = this;
       // Clear out projects
       this.name(null);
       this.repo(null);
       this.branch(null);
+      // On repo change list availableBranches
+      this.repo.subscribe(function (val) {
+        self.getBranches(val);
+      });
       // Get list
       var req = request({
         url: '/api/project/' + name
@@ -43,6 +49,45 @@ define([
       req.fail(function () {
         dom.notification('error', 'Problem loading data');
       });
+    },
+
+    // Gets branches based on current repo
+    getBranches: function (repo) {
+
+      var self = this;
+      var name;
+      this.availableBranches([]);
+
+      if (this.testRepo(repo)) {
+        var req = request({
+          url: 'api/project/branches',
+          type: 'POST',
+          payload: {
+            repo: repo
+          }
+        });
+
+        req.done(function (branches) {
+          for (var branch in branches.data) {
+            name = branches.data[branch].name.replace('origin/', '');
+            if (name !== 'HEAD') {
+              self.availableBranches.push(name);
+            }
+          }
+        });
+
+        req.fail(function () {
+          self.availableBranches([]);
+        });
+      }
+    },
+
+    // Ensure a .git extension path for repo
+    testRepo: function (repo) {
+      if (repo.split('.').pop().toLowerCase() === 'git') {
+        return true;
+      }
+      return false;
     }
 
   };
